b392ada7aeab33e888a9bff26e7fc10cc40a6c6a
Guilherme Almeida Girardi <guilhermeagirardi@gmail.com>
2023-08-27 01:36:56 -0300

    2023-08-27

--- src/Client.php
+++ src/Client.php
@@ -6,34 +6,56 @@ use Ratchet\Client\Connector;
 use React\EventLoop\Factory;
 
 class Client {
-	public function __construct(public $address, public $wait_for_response = true) {
+	public $loop;
+
+	public function __construct(public $address, public bool $wait_for_response = true, public int $waitReplyUntil = 3) {
 
 	}
 
+	public function exit($conn) {
+		$conn->close();
+		$this->loop->stop();
+	}
+
     public function __call($method, $params) {
-        $loop = Factory::create();
-        $connector = new Connector($loop);
+        $this->loop = Factory::create();
+        $connector = (new Connector($this->loop));
 
-        $connector($this->address)->then(function (\Ratchet\Client\WebSocket $conn) use ($loop, $params, $method) {
-            //Connected
-            $exit = function() use($conn, $loop) {
-                $conn->close();
-                $loop->stop();
-            };
+        $connector($this->address)
+			->then(function (\Ratchet\Client\WebSocket $conn) use ($params, $method) {
+				//Connected
 
-            $conn->on('message', function (\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn, $loop, $params, $method, $exit) {
-                $exit();
-            });
+				//Create payload
+				$payload = new RequestPayload($method, $params[0]);
 
-            $conn->send(new RequestPayload($method, $params[0]));
-            if ($this->wait_for_response)
-                $exit();
+				//Timer
+				$this->loop->addTimer($this->waitReplyUntil, function () use ($conn) {
+					throw new \Exception("No response in $this->waitReplyUntil seconds");
+				});
 
-        }, function (\Exception $e) use ($loop) {
-            //Could not connect
-            $loop->stop();
-        });
+				$conn->on('message', function (\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn, $params, $payload, $method) {
+					$response = json_decode($msg);
+					if (isset($response->id) && $response->id == $payload->id) {
+						//If there is NO an error, exit with success
+						if (empty($response->error)) {
+							$this->exit($conn);
 
-        $loop->run();
+						//If there is NO an error, throw it
+						} else
+							throw new \Exception($response->error);
+					}
+				});
+
+				//Send payload
+				$conn->send($payload);
+				if ($this->wait_for_response === false)
+					$this->exit($conn);
+
+			}, function (\Exception $e) {
+				//Could not connect
+				$this->loop->stop();
+			});
+
+        $this->loop->run();
     }
 }
